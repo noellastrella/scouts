5d68abbe84f922f09df4e0b5c5ccb2eca23aea22
Noel Lastrella <noel.lastrella@gmail.com>
2022-04-28 16:32:57 -0400

    Update partial-events.php

--- template-parts/partial-events.php
+++ template-parts/partial-events.php
@@ -24,35 +24,68 @@ if(has_post_thumbnail()){
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
 
-	<section id="content_sections">
+	<section id="content_sections" >
 		<div class="entry-content">
-			<section class="page_sections">
-				<section class="content_section">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-			<?php
-			//the_content();
-			echo get_the_title();
-			
-			wp_link_pages(
-				array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'scouts' ),
-					'after'  => '</div>',
-				)
-			);
-?>
-			</section>
-		</section>
+
 <?php
-			$title = get_field('title');
-            $startdate = get_field('start-date');
-            $enddate = get_field('start-time');
+            $event_title = get_the_title();
+            $start_date = get_field('event_start_date');
+            $end_date = get_field('event_end_date');
+            $start_time = get_field('event_start_time');
+            $end_time = get_field('event_end_time');
+            $upcoming_event_description = get_field("upcoming_event_description");
+            $event_status = get_field("event_status");
+
+			$event_description = get_field('upcoming_event_description');
+			
+			
+
+			$event_costs = get_field('event_costs');
+			$dens_participating = get_field('dens_participating');
+			
+			$cost_list = "N/A";
+			$dens_list = "N/A";
+			$event_link = get_permalink();
+			
+			if($event_costs){
+				$cost_list = "<ul class='event_listing'>";
+				foreach($event_costs as $event_costs){
+					//var_dump($event_cost);
+					$cost_list .= '<li>' . $event_costs["cost_description"] . ' : $' . $event_costs["cost_amount"] . '</li>';
+				}
+				$cost_list .= "</ul>";
+			}
+
+			if($dens_participating){
+				$dens_list = "<ul class='event_listing'>";
+				foreach($dens_participating as $den_participating){
+					//echo $den_participating;
+					$dens_list .= '<li>' . $den_participating . '</li>';
+				}
+				$dens_list .= "</ul>";
+			}
+
+			//upcoming_event_description
             //pack160_events
 echo <<<SCOUTCONTENT
-<section class="content_section" style="background-color:$bgc">
-	<h2>$header</h2>
-	<h3>$startdate $enddate</h3>
+<section class="content_section" >
+	<h2>$event_title</h2>
+	<h3>Status: $event_status</h3>
+	<h3>From: $start_date ($start_time) To: $end_date ($end_time)</h3>
+	<h3>Event Description:</h3>
 	<div class="content">
-		$text
+		$event_description
+	</div>
+	<h3>Event Costs:</h3>
+	<div class="content">
+		$cost_list
+	</div>
+	<h3>Dens Participating:</h3>
+	<div class="content">
+		$dens_list
+	</div>
+	<div>
+		<a href="$event_link">Learn More</a>
 	</div>
 </section>
 
